b8fefe24170d0df7e95c29e4be201db53017725d
Samukaell <snoooydelta37@gmail.com>
2022-12-26 22:24:27 -0300

    completo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import reset from "../styles/reset.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
